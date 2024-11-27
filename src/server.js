2aa475cc62980a360a691ea6f76ead098f95ec25
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-27 15:01:22 +0200

    add headers to connection events

--- src/server.js
+++ src/server.js
@@ -12,11 +12,11 @@ class WebSocketServer extends events.EventEmitter {
   constructor(server, options, validator = () => true) {
     super();
     server.on('upgrade', (request, socket) => {
-      if (!validRequest(request) || !validator(request.headers)) {
+      const { headers } = request;
+      if (!validRequest(request) || !validator(headers)) {
         const answer = handshake.error('Invalid Request');
         return void socket.end(answer);
       }
-      const { headers } = request;
       const key = headers['sec-websocket-key'];
       const hashed = hash(key);
       socket.write(handshake.success(hashed), 'utf8', (error) => {
@@ -24,7 +24,7 @@ class WebSocketServer extends events.EventEmitter {
         const connection = new Connection(socket);
         connection.on('disconnect', this.#onDisconnect.bind(this));
         this.#connections.add(connection);
-        this.emit('connection', connection);
+        this.emit('connection', connection, headers);
       });
     });
     server.listen(options.port, options.host);
